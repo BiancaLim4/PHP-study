50866abb8c5fdba074a050d1a10c94e66ebe51ce
Bi4nca <biancadevx@gmail.com>
2026-01-21 16:27:05 -0300

    Create Controller interface for remote control

---
+++ POO/controle-remoto-poo/Controller.php
@@ -0,0 +1,16 @@
+<?php
+
+interface Controller {
+    public function ligar();
+    public function desligar();
+    public function abrirMenu();
+    public function fecharMenu();
+    public function aumentarVolume();
+    public function baixarVolume();
+    public function ativarMudo();
+    public function desativarMudo();
+    public function play();
+    public function pause();
+}
+
+?>
